385b586e509293937a3a25ad1ea66d9be3133e93
dianaartiom <diana.artiom24@gmail.com>
2016-05-17 19:40:29 +0300

    Customized case view using bootstrap features

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,12 @@
 class PostsController < ApplicationController
-  
+  before_filter :authenticate_user!
+
   def create
     @post=Post.create(post_params)
     redirect_to posts_path
     @post.user = current_user
     @post.save
-  end  
+  end
 
   def post_params
     params.require(:post).permit!
@@ -13,10 +14,29 @@ class PostsController < ApplicationController
 
   def index
   end
-  
+
   def new
     @post = Post.new
 
+  end
+
+  def destroy
+    @post = Post.find(params[id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  def edit
+      @post = Post.find(params[:id])
+  end
+
+  def update
+      @post = Post.find(params[:id])
+      if @post.update_attributes(case_params)
+          redirect_to posts_path
+      else
+          render 'edit'
+      end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  root to: "cases#index"
   resources :posts
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
