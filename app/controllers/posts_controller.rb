8c8870462b36b65896a0bae98c2e1e3a40d19a10
dianaartiom <diana.artiom24@gmail.com>
2016-05-17 18:49:28 +0300

    Made Posts to be owned by users. Changed index view of the posts.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,15 @@
 class PostsController < ApplicationController
+  
+  def create
+    @post=Post.create(post_params)
+    redirect_to posts_path
+    @post.user = current_user
+    @post.save
+  end  
+
+  def post_params
+    params.require(:post).permit!
+  end
 
   def index
   end
