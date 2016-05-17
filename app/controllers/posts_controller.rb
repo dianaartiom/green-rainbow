0db480423b069c251a34f34da3aa7136ae2604b2
dianaartiom <diana.artiom24@gmail.com>
2016-05-17 17:31:42 +0300

    Added views for index and new methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,5 +2,10 @@ class PostsController < ApplicationController
 
   def index
   end
+  
+  def new
+    @post = Post.new
+
+  end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160517135529) do
+ActiveRecord::Schema.define(version: 20160517140403) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "image_link"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
